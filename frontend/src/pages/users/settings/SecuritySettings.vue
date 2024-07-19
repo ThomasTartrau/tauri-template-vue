1442ab610534f533ba4d604d0abcb018f1353576
ThomasTartrau <thomas.tartrau@ecoles-epsi.net>
2024-07-19 11:10:11 +0200

    feat(frontend): add a2f frontend part for enable it

--- frontend/src/pages/users/settings/SecuritySettings.vue
+++ frontend/src/pages/users/settings/SecuritySettings.vue
@@ -21,12 +21,19 @@ import {
 } from '@/components/ui/dialog'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import { displayProblem } from '@/http'
+import http, { displayError, displayProblem } from '@/http'
+import { AlertDialog, AlertDialogContent, AlertDialogTrigger, AlertDialogHeader, AlertDialogTitle, AlertDialogDescription, AlertDialogAction, AlertDialogCancel, AlertDialogFooter } from '@/components/ui/alert-dialog'
+
+const isPasswordDialogOpen = ref(false)
+const closePasswordDialog = () => isPasswordDialogOpen.value = false
+
+const isA2FDialogOpen = ref(false)
+const closeA2FDialog = () => isA2FDialogOpen.value = false
 
 const new_password = ref<string>('')
 const confirm_password = ref<string>('')
 
-async function submit() {
+async function doChangePassword() {
   if (new_password.value !== confirm_password.value) {
     return push.error({
       title: 'Invalid password',
@@ -42,8 +49,22 @@ async function submit() {
         message: 'Your password has been changed successfully',
         duration: 5000,
       })
+      closePasswordDialog()
     })
     .catch(displayProblem)
+}
+
+async function doEnableA2F() {
+  await http.post('/user/security/a2f')
+    .then(() => {
+      push.success({
+        title: 'A2F enabled',
+        message: 'Two-factor authentication has been enabled successfully',
+        duration: 5000,
+      })
+      closeA2FDialog()
+    })
+    .catch(displayError)
 }
 </script>
 
@@ -57,15 +78,15 @@ async function submit() {
         Change your password here. Please enter your new password and confirm
         it. You need to use an secure password with minimum 12 characters.
       </CardDescription>
-      <div class="flex justify-end">
-        <Dialog>
+      <div class="flex justify-end mt-4">
+        <Dialog v-model:open="isPasswordDialogOpen">
           <form>
             <DialogTrigger as-child>
               <Button variant="outline">
                 Change password
               </Button>
             </DialogTrigger>
-            <DialogContent class="sm:max-w-[425px]">
+            <DialogContent>
               <DialogHeader>
                 <DialogTitle>Change password</DialogTitle>
                 <DialogDescription>
@@ -99,13 +120,51 @@ async function submit() {
                 </div>
               </div>
               <DialogFooter>
-                <Button @click="submit">
+                <Button variant="secondary" @click="closePasswordDialog">
+                  Cancel
+                </Button>
+                <Button @click="doChangePassword">
                   Change password
                 </Button>
               </DialogFooter>
             </DialogContent>
           </form>
         </Dialog>
+      </div>
+    </CardContent>
+  </Card>
+
+  <Card>
+    <CardHeader>
+      <CardTitle> A2F </CardTitle>
+    </CardHeader>
+    <CardContent>
+      <CardDescription>
+        Enable two-factor authentication for your account. This will add an
+        additional layer of security to your account.
+      </CardDescription>
+      <div class="flex justify-end">
+        <AlertDialog v-model:open="isA2FDialogOpen">
+          <AlertDialogTrigger class>
+            <Button variant="outline">
+              Enable A2F
+            </Button>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>
+                Enable two-factor authentication
+              </AlertDialogTitle>
+              <AlertDialogDescription>
+                By enabling two-factor authentication, you need to enter a code sent to your email to log in.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction @click="doEnableA2F">Enable</AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       </div>
     </CardContent>
   </Card>
